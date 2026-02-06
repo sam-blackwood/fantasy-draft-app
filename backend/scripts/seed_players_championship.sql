b0307041bf970cc6ece475713babdd9a13d73e05
Sam Blackwood <sam.blackwood23@gmail.com>
2026-02-05 22:24:56 -0700

    revert migration to allow for passkey to be nullable

--- backend/scripts/seed_players_championship.sql
+++ backend/scripts/seed_players_championship.sql
@@ -6,8 +6,8 @@
 TRUNCATE TABLE events CASCADE;
 
 -- Create The Players 2026 event
-INSERT INTO events (name, max_picks_per_team, max_teams_per_player, status, stipulations)
-VALUES ('The Players Championship 2026', 6, 1, 'not_started', '{"tournament": "The Players", "year": 2026}'::jsonb);
+INSERT INTO events (name, max_picks_per_team, max_teams_per_player, status, passkey, stipulations)
+VALUES ('The Players Championship 2026', 6, 1, 'not_started', 'dye', '{"tournament": "The Players", "year": 2026}'::jsonb);
 
 -- Link all players to the event
 INSERT INTO event_players (event_id, player_id)
